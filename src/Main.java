547a65f9c3b01cf1075c453aa202d6adaaae869c
crhen <crhender8@gmail.com>
2024-09-03 13:21:46 -0700

    initial commit
    Signed-off-by: crhen <crhender8@gmail.com>

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.*;
+public class Main
+{
+    static char[][] board = new char[3][3];
+    public static void PrintBoard(char[][] board)
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        Arrays.fill(board, "-");
+    }
+    }
+}
