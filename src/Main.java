d7cdaf5e9bb91dea6364314ce0b670d70224b831
crhen <crhender8@gmail.com>
2024-09-05 11:55:06 -0700

    bug fixes, final version
    Signed-off-by: crhen <crhender8@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 public class Main {
     static char[][] board = new char[3][3];
-    private static char defaultSpace = '-';
-    private static char played = 'P';
-
+    //character in board before being played
+    public final static char defaultSpace = '-';
+    //character to mark played spaces
+    public final static char played = 'P';
+    //simple loop to print all board spaces in a grid
     public static void printBoard(char[][] board) {
         for(int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
@@ -16,6 +17,7 @@ public class Main {
             System.out.println();
         }
     }
+    //checks if all spaces are filled
     public static boolean checkContinue(char[][] board) {
         for(int row = 0; row < 3; row++) {
             for(int col = 0; col < 3; col++) {
@@ -26,6 +28,7 @@ public class Main {
         return false;
     }
     public static void main(String[] args) {
+        //fills the board with the default character
         for(int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
                 board[row][col] = defaultSpace;
@@ -33,25 +36,38 @@ public class Main {
         }
         Scanner scanner = new Scanner(System.in);
         boolean cont = checkContinue(board);
+        //loop repeatedly takes input, fills appropriate board space, and prints board
         while(cont) {
             printBoard(board);
+            if(!checkContinue(board)) {
+                cont = checkContinue(board);
+                System.out.println("Congratulations! You have won the game!");
+                continue;
+            }
             System.out.print("Enter a row and a column, from 0 to 2, or press q to quit: ");
-            if (scanner.hasNextInt() && scanner.nextInt() <= 2 && scanner.nextInt() >= 0) {
+            //note that input must be separated by a space
+            if (scanner.hasNextInt()) {
                 int row = scanner.nextInt();
-                if (scanner.hasNextInt() && scanner.nextInt() <= 2 && scanner.nextInt() >= 0) {
+                if (scanner.hasNextInt()) {
                     int col = scanner.nextInt();
+                    if (row < 0 || row >= 3 || col < 0 || col >= 3) {
+                        System.out.println("Invalid row or column");
+                        continue;
+                    }
+                    if (board[row][col] != defaultSpace) {
+                        System.out.println("That space is already taken");
+                        continue;
+                    }
                     board[row][col] = played;
-                    return;
+                    continue;
                 }
             }
             cont = checkContinue(board);
+            //option to quit at any time
             if(Objects.equals(scanner.next(), "q")) {
                 cont = false;
                 System.out.println("thanks for playing!");
-                return;
             }
-            else
-                System.out.println("Invalid input");
         }
     }
     }
